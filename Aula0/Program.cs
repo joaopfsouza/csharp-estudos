cdac1bf47cc7f5848f102e067cc0faf91f48936b
João Paulo Ferreira de Souza <joao.ferreiradesouza@hotmail.com>
2019-04-05 00:54:44 -0300

    Estudos

---
+++ Aula0/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace Aula0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("CurrentCulture is {0}.", CultureInfo.CurrentCulture.Name);
+
+        
+            Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-US");
+
+            System.Console.WriteLine("Insira informações do primeiro produto:");
+            string[] productOne = Console.ReadLine().Split(' ');
+
+            System.Console.WriteLine("Segundo Produto:");
+            string[] productTwo = Console.ReadLine().Split(' ');
+
+            double costProductOne = Double.Parse(productOne[2]);
+            double costProductTwo = Double.Parse(productTwo[2]);
+
+    
+            double result = Convert.ToInt32(productOne[1]) * costProductOne
+                                + Convert.ToInt32(productTwo[1]) * costProductTwo;
+
+            System.Console.WriteLine($"Valor total: $ {result}");
+
+
+
+        }
+
+
+        static decimal Amount(int numberUnit, decimal costProduct)
+        {
+
+            return numberUnit * costProduct;
+        }
+    }
+}
