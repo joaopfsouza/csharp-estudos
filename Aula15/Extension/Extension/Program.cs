15ec60b280bc383cec2daa29e2bc10faa9864ccc
João Paulo Ferreira de Souza <joao.ferreiradesouza@hotmail.com>
2019-05-15 22:10:15 -0300

    Extensions

---
+++ Aula15/Extension/Extension/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2019, 05, 15, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+
+            //string s1 = "Good morning dear students!";
+            string s1 = "Good";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
