d63fe1e2fa7e0abbeb74c248fd056d1bb9efa2ad
João Paulo Ferreira de Souza <joao.ferreiradesouza@hotmail.com>
2019-05-24 09:26:06 -0300

    Linq alternativo

--- Aula16/Linq/Linq/Program.cs
+++ Aula16/Linq/Linq/Program.cs
@@ -28,6 +28,90 @@ namespace Linq
                 new Product(){Id=11, Name="Level", Price=70.0,Category=c1}
             };
 
+            var r1 =
+                    from p in products
+                    where p.Category.Tier == 1 && p.Price < 900
+                    select p;
+            Print("TIER 1 AND PRICE < 900:", r1);
+
+            var r2 =
+                    from p in products
+                    where p.Category.Name == "Tools"
+                    select p.Name;
+
+            Print("NAMES OF PRODUCTS FROM TOOLS", r2);
+
+
+            var r3 =
+                from p in products
+                where p.Name[0] == 'C'
+                select new
+                {
+                    ProductName = p.Name,
+                    p.Price,
+                    Category = p.Category
+                };
+
+            Print("NAMES STARTED WITH 'C' AND ANONYMOS OBJECT", r3);
+
+
+            var r4 =
+                    from p in products
+                    where p.Category.Tier == 1
+                    orderby p.Name
+                    orderby p.Price
+                    select p;
+
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME", r4);
+
+            var r5 =
+                (from p in r4
+                 select p).Skip(2).Take(4);
+
+            Print("TIER 1 ORDER BY PRICE THEN BY NAME SKIP 2 TAKE 4", r5);
+
+
+            var r7 = (from p in products select p).FirstOrDefault();
+            Console.WriteLine("FIRST OR DEFAULT 1: {0}", r7);
+
+
+            var r18 =
+                from p in products
+                group p by p.Category;
+
+            foreach (IGrouping<Category, Product> group in r18)
+            {
+                Console.WriteLine("Category " + group.Key.Name + ":");
+
+                foreach (Product p in group)
+                {
+                    Console.WriteLine(p);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static void LinqPadrao()
+        {
+            Category c1 = new Category() { Id = 1, Name = "Tools", Tier = 2 };
+            Category c2 = new Category() { Id = 2, Name = "Computers", Tier = 1 };
+            Category c3 = new Category() { Id = 3, Name = "Eletronics", Tier = 1 };
+
+            List<Product> products = new List<Product>()
+            {
+                new Product(){Id=1, Name="Computer", Price=1100.0,Category=c2},
+                new Product(){Id=2, Name="Hammer", Price=90.0,Category=c1},
+                new Product(){Id=3, Name="TV", Price=1700.0,Category=c3},
+                new Product(){Id=4, Name="Notebook", Price=1300.0,Category=c2},
+                new Product(){Id=5, Name="Saw", Price=80.0,Category=c1},
+                new Product(){Id=6, Name="Tablet", Price=700.0,Category=c2},
+                new Product(){Id=7, Name="Camera", Price=700.0,Category=c3},
+                new Product(){Id=8, Name="Printer", Price=350.0,Category=c3},
+                new Product(){Id=9, Name="MacBook", Price=1800.0,Category=c2},
+                new Product(){Id=10, Name="Sound Bar", Price=700.0,Category=c3},
+                new Product(){Id=11, Name="Level", Price=70.0,Category=c1}
+            };
+
             var r1 = products.Where(p => p.Price < 900 && p.Category.Tier == 1);
             Print("TIER 1 AND PRICE < 900:", r1);
 
@@ -87,9 +171,9 @@ namespace Linq
 
 
             var r18 = products.GroupBy(p => p.Category);
-            foreach (IGrouping<Category,Product> group in r18)
+            foreach (IGrouping<Category, Product> group in r18)
             {
-                Console.WriteLine("Category "  + group.Key.Name + ":");
+                Console.WriteLine("Category " + group.Key.Name + ":");
 
                 foreach (Product p in group)
                 {
@@ -97,7 +181,6 @@ namespace Linq
                 }
                 Console.WriteLine();
             }
-
         }
 
         private static void Print<T>(string message, IEnumerable<T> collection)
